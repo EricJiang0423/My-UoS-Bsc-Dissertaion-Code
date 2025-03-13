460d24c962e7cd5683e1ce5206761efb9111b081
EricJiang0423 <1327449489@qq.com>
2025-03-13 19:47:27 +0000

    new reward function

--- config.py
+++ config.py
@@ -11,7 +11,7 @@ TRAIN_START_DATE = "2017-11-09"
 TRAIN_END_DATE = "2023-12-31"
 
 TEST_START_DATE = "2024-01-01"
-TEST_END_DATE = "2025-03-10"
+TEST_END_DATE = "2025-03-01"
 
 # stockstats technical indicator column names
 # check https://pypi.org/project/stockstats/ for different names
